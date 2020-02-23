66d40c0bef1dfa52b4cb294d85cb9b22a111ef69
Brad Barnhill <bradfordbarnhill@gmail.com>
2020-02-23 17:55:15 -0600

    gradle 6.2, fix pharmacode single digit encoding

--- src/main/java/com/pnuema/java/barcode/symbologies/Pharmacode.java
+++ src/main/java/com/pnuema/java/barcode/symbologies/Pharmacode.java
@@ -41,42 +41,29 @@ public class Pharmacode extends BarcodeCommon implements IBarcode {
             error("EPHARM-4: Data contains invalid  characters (invalid numeric range).");
         }
 
-        int startIndex = 0;
-
-        //find start index
-        for (int index = 15; index >= 0; index--) {
-            if (Math.pow(2, index) < num / 2) {
-                startIndex = index;
-                break;
-            }
-        }
-
-        double sum = Math.pow(2, startIndex + 1) - 2;
-        String[] encoded = new String[startIndex + 1];
-        int i = 0;
-
         String thickBar = "111";
         String thinBar = "1";
-        for (int index = startIndex; index >= 0; index--) {
-            double power = Math.pow(2, index);
-            double diff = num - sum;
-            if (diff > power) {
-                encoded[i++] = thickBar;
-                sum += power;
-            } else {
-                encoded[i++] = thinBar;
-            }
-        }
-
         String gap = "00";
         StringBuilder result = new StringBuilder();
-        for (String s : encoded) {
-            if (result.length() > 0) {
-                result.append(gap);
+
+        do
+        {
+            if ((num & 1) == 0)
+            {
+                result.insert(0, thickBar);
+                num = (num - 2) / 2;
+            }
+            else
+            {
+                result.insert(0, thinBar);
+                num = (num - 1) / 2;
             }
 
-            result.append(s);
-        }
+            if (num != 0)
+            {
+                result.insert(0, gap);
+            }
+        } while (num != 0);
 
         return result.toString();
     }
