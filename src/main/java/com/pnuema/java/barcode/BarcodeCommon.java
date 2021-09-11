b771a507f6a16204ba1ad033baeaf5fbd413aa3f
bradbarnhill <bradfordbarnhill@gmail.com>
2021-09-11 16:46:05 -0500

    update dependencies

--- src/main/java/com/pnuema/java/barcode/BarcodeCommon.java
+++ src/main/java/com/pnuema/java/barcode/BarcodeCommon.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public abstract class BarcodeCommon {
     private String rawData = "";
-    private List<String> errors = new ArrayList<>();
+    private final List<String> errors = new ArrayList<>();
 
     protected void setRawData(String rawData) {
         this.rawData = rawData;
