9636b6fabc83117ed6768102d6b47759fb79c732
Brad Barnhill <bradfordbarnhill@gmail.com>
2024-02-17 23:35:03 -0600

    allow building without remote cache

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -28,7 +28,8 @@ gradleEnterprise {
     }
 }
 
-val cacheUrl: String? = if (System.getenv("REMOTE_CACHE_URL") == null) extra["REMOTE_CACHE_URL"] as String else System.getenv("REMOTE_CACHE_URL")
+val remoteCacheUrl: String? by extra
+val cacheUrl: String? = if (System.getenv("REMOTE_CACHE_URL") == null) remoteCacheUrl else System.getenv("REMOTE_CACHE_URL")
 
 if (cacheUrl != null) {
     buildCache {
@@ -43,11 +44,14 @@ if (cacheUrl != null) {
             isAllowUntrustedServer = true
             isAllowInsecureProtocol = false
             if (isEnabled) {
-                println("Using remote build cache: " + cacheUrl)
+                println("Using remote build cache: $cacheUrl")
             }
+
+            val remoteCacheUser: String? by extra
+            val remoteCachePass: String? by extra
             credentials {
-                username = if (System.getenv("REMOTE_CACHE_USER") == null) extra["REMOTE_CACHE_USER"] as String else System.getenv("REMOTE_CACHE_USER")
-                password = if (System.getenv("REMOTE_CACHE_PASS") == null) extra["REMOTE_CACHE_PASS"] as String else System.getenv("REMOTE_CACHE_PASS")
+                username = if (System.getenv("REMOTE_CACHE_USER") == null) remoteCacheUser else System.getenv("REMOTE_CACHE_USER")
+                password = if (System.getenv("REMOTE_CACHE_PASS") == null) remoteCachePass else System.getenv("REMOTE_CACHE_PASS")
             }
         }
     }
